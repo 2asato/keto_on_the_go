a7ab351e73884a2fa750e74da273a6de595ccec4
aaronsato <2asato@gmail.com>
2020-04-16 16:18:40 -0700

    created routes directory and refactored location routes

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var express = require('express'),
     User = require('./models/user')
     seedDB = require('./seeds');
 
+var locationRoutes = require('./routes/locations')
+
 // connect mongoose
 mongoose.connect('mongodb://localhost/seattle_on_keto', { useNewUrlParser: true, useUnifiedTopology: true });
 app.use(bodyParser.urlencoded({extended: true}));
@@ -48,85 +50,6 @@ app.get('/', function(req, res) {
 })
 
 
-// ======================
-// locations routes
-// =======================
-
-// locations index route
-app.get('/locations', function(req, res) {
-    Location.find({}, function(err, allLocations) {
-        if(err) {
-            console.log(err);
-            
-        } else {
-            res.render('locations/index', { locations: allLocations, currentUser: req.user });
-
-        }
-    })
-})
-
-// locations create route
-app.post('/locations', function(req, res) {
-    Location.create(req.body.location, function(err, newLocation) {
-        if(err) {
-            console.log(err);
-            res.render('locations/new')
-        } else {
-            newLocation.save();
-            console.log(newLocation);
-            res.redirect('locations')
-        }
-    })
-})
-
-// locations new route
-app.get('/locations/new', function(req, res) {
-    res.render('locations/new')
-})
-
-// location show route
-app.get('/locations/:id', function(req, res) {
-    Location.findById(req.params.id).populate('comments').exec(function(err, foundLocation) {
-        if(err) {
-            console.log(err);
-            res.redirect('back')
-        } else {
-            console.log(foundLocation);
-            res.render('locations/show', { location: foundLocation })
-        }
-    })
-})
-
-// location edit route
-app.get('/locations/:id/edit', function(req, res) {
-    Location.findById(req.params.id, function(err, foundLocation) {
-        res.render('locations/edit', { location: foundLocation })
-    })
-})
-
-// location update route
-app.put('/locations/:id', function(req, res) {
-    // find and update correct location
-    Location.findByIdAndUpdate(req.params.id, req.body.location, function(err, updatedLocation) {
-        if(err) {
-            res.redirect('/locations');
-        } else {
-            res.redirect('/locations/' + req.params.id)
-        }
-    })
-})
-
-// location delete route
-app.delete('/locations/:id', function(req, res) {
-    // find and delete correct location
-    Location.findByIdAndRemove(req.params.id, function(err, ) {
-        if(err) {
-            res.redirect('/loations')
-        } else {
-            res.redirect('/locations')
-        }
-    })
-})
 
 
 // ===================
@@ -227,7 +150,8 @@ function isSignedIn(req, res, next) {
 }
 
 
-
+// requiring routes
+app.use(locationRoutes);
 
 
 // Tell Express to listen for requests (start server)
---
+++ routes/locations.js
@@ -0,0 +1,94 @@
+var express = require('express'),
+    router = express.Router(),
+    Location = require('../models/location');
+
+// ======================
+// locations routes
+// =======================
+
+// locations index route
+router.get('/locations', function(req, res) {
+    Location.find({}, function(err, allLocations) {
+        if(err) {
+            console.log(err);
+            
+        } else {
+            res.render('locations/index', { locations: allLocations, currentUser: req.user });
+
+        }
+    })
+})
+
+// locations create route
+router.post('/locations', function(req, res) {
+    Location.create(req.body.location, function(err, newLocation) {
+        if(err) {
+            console.log(err);
+            res.render('locations/new')
+        } else {
+            newLocation.save();
+            console.log(newLocation);
+            res.redirect('locations')
+        }
+    })
+})
+
+// locations new route
+router.get('/locations/new', function(req, res) {
+    res.render('locations/new')
+})
+
+// location show route
+router.get('/locations/:id', function(req, res) {
+    Location.findById(req.params.id).populate('comments').exec(function(err, foundLocation) {
+        if(err) {
+            console.log(err);
+            res.redirect('back')
+        } else {
+            console.log(foundLocation);
+            res.render('locations/show', { location: foundLocation })
+        }
+    })
+})
+
+// location edit route
+router.get('/locations/:id/edit', function(req, res) {
+    Location.findById(req.params.id, function(err, foundLocation) {
+        res.render('locations/edit', { location: foundLocation })
+    })
+})
+
+// location update route
+router.put('/locations/:id', function(req, res) {
+    // find and update correct location
+    Location.findByIdAndUpdate(req.params.id, req.body.location, function(err, updatedLocation) {
+        if(err) {
+            res.redirect('/locations');
+        } else {
+            res.redirect('/locations/' + req.params.id)
+        }
+    })
+})
+
+// location delete route
+router.delete('/locations/:id', function(req, res) {
+    // find and delete correct location
+    Location.findByIdAndRemove(req.params.id, function(err, ) {
+        if(err) {
+            res.redirect('/loations')
+        } else {
+            res.redirect('/locations')
+        }
+    })
+})
+
+// middleware
+function isSignedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect('/signin');
+}
+
+
+module.exports = router;
