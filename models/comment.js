2d9d6dfec112c682ef766a3dad8ecb47fcf3a91a
aaronsato <2asato@gmail.com>
2020-04-17 12:45:23 -0700

    changed comment model, updated author

--- models/comment.js
+++ models/comment.js
@@ -3,7 +3,13 @@ var mongoose = require('mongoose');
 // schema
 var commentSchema = new mongoose.Schema({
     text: String,
-    author: String,
+    author: {
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        username: String
+    },
     added: { type: Date, default: Date.now }
 })
 
