28fa8cc8fe326a6dd790aa928338612574e7392c
aaronsato <2asato@gmail.com>
2020-05-21 13:58:08 -0700

    created new image route

--- routes/images.js
+++ routes/images.js
@@ -2,3 +2,19 @@ var express = require('express'),
     router = express.Router({mergeParams: true}),
     Location = require('../models/location'),
     middleware = require('../middleware')
+
+
+// new image route
+router.get('/locations/:id/images/new', function(req, res) {
+    Location.findById(req.params.id, function(err, location) {
+        if (err) {
+            console.log(err);
+            
+        } else {
+            res.send('soon to be new image form page')
+        }
+    })
+})
+
+
+module.exports = router;
